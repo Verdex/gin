b2d081b58d40572be336e8f09e35027a8aa9bca1
David L. Inman <davidlevere@gmail.com>
2022-05-03 22:14:00 -0400

    path test

--- src/data_pattern.rs
+++ src/data_pattern.rs
@@ -100,6 +100,67 @@ impl<'a> Iterator for Paths<'a> {
 mod test {
     use super::*;
 
+    macro_rules! proj {
+        ($input:expr => $pattern:pat => $($output:tt)+) => {
+            let $($output)+ = match $input {
+                $pattern => $($output)+,
+                _ => panic!("projection failure"),
+            };
+        };
+    }
+
+    #[test]
+    fn path_should_create_paths() {
+        let input = Tree::Node(
+            Box::new(Tree::Leaf(1)),
+            Box::new(
+                Tree::Node(
+                    Box::new(Tree::Leaf(2)),
+                    Box::new(Tree::Leaf(3))
+                )
+            )
+        );
+
+        let paths = Paths::new(&input).into_iter().collect::<Vec<_>>();
+
+        assert_eq!( paths.len(), 3 );
+
+        // path 1
+        assert_eq!( paths[0].len(), 2 );
+        proj!( paths[0][0] => Tree::Node( a00, b00 ) => (a00, b00) );
+
+        assert!( matches!( **a00, Tree::Leaf(1) ) );
+        assert!( matches!( **b00, Tree::Node(_, _) ) );
+
+        assert!( matches!( paths[0][1], Tree::Leaf(1) ) );
+
+        // path 2
+        assert_eq!( paths[1].len(), 3 );
+        proj!( paths[1][0] => Tree::Node( a10, b10 ) => (a10, b10) );
+
+        assert!( matches!( **a10, Tree::Leaf(1) ) );
+        assert!( matches!( **b10, Tree::Node(_, _) ) );
+
+        proj!( paths[1][1] => Tree::Node( a11, b11 ) => (a11, b11) );
+        assert!( matches!( **a11, Tree::Leaf(2) ) );
+        assert!( matches!( **b11, Tree::Leaf(3) ) );
+
+        assert!( matches!( paths[1][2], Tree::Leaf(2) ) );
+
+        // path 3
+        assert_eq!( paths[2].len(), 3 );
+        proj!( paths[2][0] => Tree::Node( a20, b20 ) => (a20, b20) );
+
+        assert!( matches!( **a20, Tree::Leaf(1) ) );
+        assert!( matches!( **b20, Tree::Node(_, _) ) );
+
+        proj!( paths[1][1] => Tree::Node( a21, b21 ) => (a21, b21) );
+        assert!( matches!( **a21, Tree::Leaf(2) ) );
+        assert!( matches!( **b21, Tree::Leaf(3) ) );
+
+        assert!( matches!( paths[2][2], Tree::Leaf(3) ) );
+    }
+
     #[test]
     fn blarg() {
         let x = Tree::Node(
