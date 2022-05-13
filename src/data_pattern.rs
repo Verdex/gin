ce63a85451076b4a2ee32ca2a7bd03f5c868b3b2
David L. Inman <davidlevere@gmail.com>
2022-05-12 23:45:20 -0400

    clean up note

--- src/data_pattern.rs
+++ src/data_pattern.rs
@@ -1,12 +1,3 @@
-/*
-    It's a depth first search except you keep track of each item you see 
-    then return those items at the bottom
-
-    However, you also need to keep some of the items and drop others
-    each time you backtrack and take a new path
-
-
-*/
 
 #[derive(Debug)]
 enum Tree {
