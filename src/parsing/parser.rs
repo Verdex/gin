54e4367d017034244e635797d310b3e46dafc589
David L. Inman <davidlevere@gmail.com>
2022-05-27 00:08:15 -0400

    parse_type:  concrete and generic parser

--- src/parsing/parser.rs
+++ src/parsing/parser.rs
@@ -1,8 +1,48 @@
 
-use crate::data::{TMeta, Token};
+use crate::{alt, group, pred, seq, cases};
+use crate::array_pattern::MatchError;
+use crate::data::{TMeta, Token, AMeta, Type};
 
 pub fn parse(tokens : Vec<Token>) -> Result<(), String> {
 
     Err("TODO".into())
+}
+
+group!(parse_type: Token => Type = |input| {
+    seq!(concrete: Token => Type = name <= Token::UpperSymbol(_, _), {
+        if let Token::UpperSymbol(meta, name) = name {
+            let ameta = AMeta { token_meta: vec![meta] };
+            Type::ConcreteType(ameta, name)
+        }
+        else {
+            panic!("Expected UpperSymbol");
+        }
+    });
+
+    seq!(generic: Token => Type = name <= Token::LowerSymbol(_, _), {
+        if let Token::LowerSymbol(meta, name) = name {
+            let ameta = AMeta { token_meta: vec![meta] };
+            Type::ConcreteType(ameta, name)
+        }
+        else {
+            panic!("Expected LowerSymbol");
+        }
+    });
+
+    alt!(main: Token => Type = generic 
+                             | concrete
+                             );
+
+    main(input)
+});
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn should() {
+
+    }
 }
 
