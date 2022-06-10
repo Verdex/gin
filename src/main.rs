690aa4521a23deca2c60524304d6d360f327746c
David L. Inman <davidlevere@gmail.com>
2022-06-09 22:59:32 -0400

    feed token into parser

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ mod parsing;
 
 fn main() {
 
-    let _token_results = parsing::tokenizer::tokenize("");
-    println!("Hello, world!");
+    let input = "";
+    let token_results = parsing::tokenizer::tokenize(input);
+    match token_results {
+        Ok(tokens) => {
+            let _ast_result = parsing::parser::parse(input, tokens);
+        },
+        Err(message) => println!("{}", message),
+    }
 }
